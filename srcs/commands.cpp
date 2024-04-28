14ef9cbc8fee3b9382145d1c30f7a8725025b1e2
selrhair <soufiane.elrhair@gmail.com>
2024-04-28 17:47:45 +0100

    update

--- srcs/commands.cpp
+++ srcs/commands.cpp
@@ -139,6 +139,21 @@ std::string Server::user(Request request, int fd)
         }
     this->client[fd]->setusername(request.arguments[0]);
     return ("IRC : Your username is " + request.arguments[1]);
+        // if (client->is_registered())
+    // {
+    //     client->reply(ERR_ALREADYREGISTERED(client->get_nickname()));
+    //     return;
+    // }
+
+    // if (args.size() < 4) 
+    // {
+    //     client->reply(ERR_NEEDMOREPARAMS(client->get_nickname(), "USER"));
+    //     return;
+    // }
+
+    // client->set_username(args[0]);
+    // client->set_realname(args[3]);
+    // client->welcome();
 }
 
 std::string Server::join(Request request, int fd)
